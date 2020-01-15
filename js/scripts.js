1c61e5e78cd58c82085eb59e3cb71fb70dccf670
Daniel Schuman <making_melodies93@yahoo.com>
2020-01-15 14:05:17 -0800

    Add modal window click event

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
-//fetch data from API
-fetch('https://randomuser.me/api/?results=12')
-  .then(res => res.json())
-//  .then(data => console.log(data))
-  .then(data => generateCards(data.results))
-
-
 //global variables
 const galleryDiv = document.getElementById('gallery');
 const body = document.querySelector('body');
-let cards = document.querySelectorAll('div.card');
+let peopleArray = [];
+
+
+//fetch data from API
+fetch('https://randomuser.me/api/?nat=us&results=12')
+  .then(res => res.json())
+  .then(data => {
+    peopleArray = [...data.results];
+    generateCards(data.results);})
 
 
 //function to generate HTML
@@ -31,27 +32,39 @@ function generateCards(employeeList) {
 
 //function to create modal window
 function createModal(data) {
-  let window = `<div class="modal-container">
-                <div class="modal">
+  let window = document.createElement('div');
+  window.className = 'modal-container';
+  window.innerHTML = 
+                `<div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                     <div class="modal-info-container">
-                        <img class="modal-img" src=${data.picture.medium} alt="profile picture">
+                        <img class="modal-img" src=${data.picture.large} alt="profile picture">
                         <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
                         <p class="modal-text">${data.email}</p>
                         <p class="modal-text cap">${data.location.city}</p>
                         <hr>
-                        <p class="modal-text">data.phone</p>
+                        <p class="modal-text">${data.phone}</p>
                         <p class="modal-text">${data.location.street.number} ${data.location.street.name}, ${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
-                        <p class="modal-text">data.dob.date</p>
+                        <p class="modal-text">Birthday: ${data.dob.date.slice(5,7)}/${data.dob.date.slice(8,10)}/${data.dob.date.slice(0,4)}</p>
                     </div>
                 </div>`
+  ;
   body.appendChild(window);
+  window.style.display = 'block';
+  
+  //event handler to close modal window
+  const closeBtn = document.getElementById('modal-close-btn');
+  closeBtn.addEventListener('click', () => {
+    if (window.style.display === 'block'){
+      window.style.display = 'none';
+    }
+  });
 }
 
 
 //event listener to display modal window
-for(i=0; i<cards.length; i++){
-  cards[i].addEventListener('click', (e) => {
-
+galleryDiv.addEventListener('click', (e) => {
+  if (e.target.className.includes('card')) {
+    createModal(peopleArray[0]);
+  }
   });
-}
